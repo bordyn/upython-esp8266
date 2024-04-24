dbce8534a043fc8bba63bba5561f44ced4db84fe
bordyn <bordyn.ch@gmail.com>
2024-04-24 17:50:17 +0700

    updated

---
+++ workspace/basics/7.basic_condition.py
@@ -0,0 +1,15 @@
+a = 100
+
+# 1. show by if condition that a == 100
+# else print a is not 100
+
+# 2. compare x to "Joe"
+x = "Joe"
+
+# 3. check if 4 member is in b , by if 4 in b
+b = [4,5,6,7]
+
+# 4. show nested if by checking "name" as b's member (checking keys),
+# subsequently also show that "student_a" is in b values
+b = {"name":"student_a","score":3.0}
+    
