d8ef73388faa701c0d56b9ecfbf22d05bb836257
bordyn <bordyn.ch@gmail.com>
2024-04-24 17:28:34 +0700

    updated

---
+++ workspace/basics/3.basic_operator.py
@@ -0,0 +1,13 @@
+a = 1
+b = 2
+
+# 1. show a greater b comparison
+
+# 2. show a less b comparision
+
+# 3. show b equal c comparison
+c = 2
+
+# 4. show b greater or equal c
+
+# 5. show b less or equal c
