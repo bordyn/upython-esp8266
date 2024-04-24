f051e24966f6b430f96b6c9dd2ba5d6c8da785f8
bordyn <bordyn.ch@gmail.com>
2024-04-24 18:46:10 +0700

    updated

---
+++ workspace/basics/10.basic_function.py
@@ -0,0 +1,19 @@
+# 1. show basic function that return the same value 
+
+# 2. show the function that receive list and appends 4 into argument
+# call the function using a as argument.
+a = [1,2,3]
+
+# 3. show the function that accepts 2 argument and return the sum
+a = [1,2,3]
+b = [4,5,6]
+
+# 4. show the function that contain default in argument,
+# try using func_5 with a only and a,10 
+a = 10
+def  func_5(a,b=5):
+    return  a*2 + b
+
+
+
+    
---
+++ workspace/basics/11.basic_module.py
@@ -0,0 +1,7 @@
+# 1. try importing quad
+
+# 2. save quad.py in basic_modules in esp8266 file.
+# import quad
+# this equals to x**2 + x + 1 ,where x = 2
+# print (quad.quardratic_fn(1,1,1,2))
+
