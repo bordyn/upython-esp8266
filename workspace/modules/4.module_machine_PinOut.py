adc245f157a738201e48d4b43ec4600a7c3dbef6
bordyn <bordyn.ch@gmail.com>
2024-04-24 18:53:40 +0700

    updated

---
+++ workspace/modules/4.module_machine_PinOut.py
@@ -0,0 +1,10 @@
+from time import sleep
+from machine import Pin
+
+# 1. turning LED Pin2 on/off board using led=Pin(2, Pin.OUT)
+
+# 2. turning LED Pin16 on/off board using led=Pin(16, Pin.OUT)
+
+    
+    
+    
