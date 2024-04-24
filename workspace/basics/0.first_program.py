02c86904388c6897194875dfaed6eebc6003d20a
bordyn <bordyn.ch@gmail.com>
2024-04-24 17:18:49 +0700

    add workspace

---
+++ workspace/basics/0.first_program.py
@@ -0,0 +1,21 @@
+print ("Hello World")
+
+# first thing first
+# Basic guideline for writing any program
+# 1. Any name specified to something should not contain white space
+#    colon,semi-colon slash or back slash ,full stop.
+#    These characters are reserved for special purpose
+# 2. arithmetic sign will be reserved for arithmetic operations.
+#    +,-,*,/,%,//
+# 3. A white space define complete variable name or any other program entity.
+# 4. Always be case sensitive.
+# 5. Any programing language has their own "keywords". Try to remember them and their usage.
+#    even though ,you rarely use some of them.
+# 6. Avoide using any keyword as variable name.
+# 7. white spaces and tabs are not replaceable for some language. especially python
+
+# Suggested Guideline for writing any program.
+# 1. Keep program indentation.
+# 2. Always define a series of job as function , do not write a very long function
+#    (e,g more than 100 lines of code).
+# 3. Keep track of variable type, especially with the flexible type program.
---
+++ workspace/basics/1.basic_math.py
@@ -0,0 +1,17 @@
+a = 1
+b = 3.5
+c = -2
+d = 2
+
+# 1.show a+b
+# 2.show a-b
+# 3.show a*b
+# 4.show a/b
+# 5.show c**d
+
+a = 5
+b = 2
+# 6.show floor division a//b
+# 7.show modulus, a%b return remainder
+
+# show devide by zero a/0
