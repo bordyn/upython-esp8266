689782f5b135c98f51c9441555c1db69a4c289e9
bordyn <bordyn.ch@gmail.com>
2024-04-24 18:36:09 +0700

    updated

---
+++ workspace/basics/8.basic_for.py
@@ -0,0 +1,24 @@
+# 1. create for loop that print number in range(0,10)
+
+# 2. create python comprehension list of number in range(0,10) as x_list 
+
+# 3. iterate through b using for k,v in b.items()
+b = {"name":"student_a","score":3.0}
+
+# 4. iterate through list of num, using for n in num:
+num = [1,2,3,4,5]
+
+# 5. show break statement for n in num , if n == 4 then break ,
+# showing iteration by printing n
+
+# 6. showing else statement for n in num , if n == 6 then break
+# else at the end of loop , showing that n==6 is not valid.
+
+# 7. continue statement for 'for' 
+# show for n in num if n is even (n%2==0) then skip by continue
+# else add to odd_num_list 
+odd_num_list = []
+
+        
+        
+        
