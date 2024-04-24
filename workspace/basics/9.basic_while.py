de51b41f59c6e0b64a229fb869a7bd4076930634
bordyn <bordyn.ch@gmail.com>
2024-04-24 18:38:56 +0700

    updated

---
+++ workspace/basics/9.basic_while.py
@@ -0,0 +1,11 @@
+# 1. show basic while that keeps on printing incremental count,
+# while count < 5
+count = 0
+
+# 2. show if an item in x is greater than or equal to 5 ,then break
+# using while count < len(x)
+x = [1,2,3,4,5,6,7,8,9,10] #1-10
+count = 0
+
+# 3. showling while True: can be used as infinite loop
+    
