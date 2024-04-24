4dca2d18799b09096995b5becb5a4868fcadb4c1
bordyn <bordyn.ch@gmail.com>
2024-04-24 17:43:02 +0700

    updated

---
+++ workspace/basics/6.basic_dict.py
@@ -0,0 +1,25 @@
+a = {"name":"student a","age":20,"score":3.0}
+
+# 1. simply print each element in a
+# a["name"],a["age"],a["score"]
+
+# 2. try access to something does not exist
+# a['class']
+
+# 3. try adding 'class' with 'class_1' to dictionary a
+
+# 4. accessing 'score' in dictionary a
+
+# 5. show all all keys of dictionary a
+
+# 6. show all values of dictionary a
+
+# 7. show all items of dictionary a
+
+# 8. removing an element using key "name"
+# del a["name"]
+
+# 9. replacing a["name"] as student "student_a"
+
+# 10. get dictionary with default value
+# a.get("name","No name")
