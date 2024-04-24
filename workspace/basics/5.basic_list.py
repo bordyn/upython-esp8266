159dab888dfb1c87c96824143a41c86918846491
bordyn <bordyn.ch@gmail.com>
2024-04-24 17:36:28 +0700

    updated

---
+++ workspace/basics/5.basic_list.py
@@ -0,0 +1,30 @@
+a = ["car","bike","plane"]
+b = [1,2,3.1,4.1]
+
+# 1. simply print a 
+# 2. show a and b types
+
+# 3. show referring index 0 in a, index 0 in b
+# 4. show referring index 2 in a, index 2 in b
+
+# 5. show list can not exceed its length a[3]
+
+# 6. find a's length
+
+# 7. show adding "rocket" in a
+
+# 8. show removing "car" from a
+
+# 9. insert "car" item into a at 0
+
+# 10. list can be added (concatenated) with different type
+# show a,b concatenation and store to c
+
+# 11. show adding b as member to a , a.append(b) 
+
+# 12. remove appended b from a , a.remove(b)
+
+# 13. check if an element qualify something,
+# create list comprehension that shows item in b > 3
+
+
