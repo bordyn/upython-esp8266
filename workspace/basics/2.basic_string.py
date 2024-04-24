b97b31a73d774cdc0a51fa0fa254d4093907035c
bordyn <bordyn.ch@gmail.com>
2024-04-24 17:22:15 +0700

    updated

---
+++ workspace/basics/2.basic_string.py
@@ -0,0 +1,26 @@
+a = "Hello I'm a Bascii student"
+b = 'for so long'
+
+# 1. show string a
+# 2. show string b
+
+# 3. string can be added (concatenated) , show c=a + b
+
+# 4. simple way to combine string and integer
+# show a combine to integer 1
+
+# 5. show unsupported string subtraction
+# c = a - b
+# print (c)
+
+# 6. show a way to subtract some element from string
+# replace a with d = "I'm not a"
+# c = a.replace(d,'')
+
+# 7. find an index of character in a string
+# d = 'a'
+# show a.index(d)
+
+# 8. check if some character(s) in a string?
+# 'Z' in a
+# 'I' in a
